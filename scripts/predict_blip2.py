67f2201d2ea1524aec02e36a0080efaa70389bd0
segis95 <segis95.ai@gmail.com>
2023-10-06 23:33:20 +0000

    Added data loader and accelerate to inference script

--- scripts/predict_blip2.py
+++ scripts/predict_blip2.py
@@ -3,74 +3,143 @@ import pandas as pd
 from tqdm import tqdm
 from PIL import Image
 import hydra
-import wandb
 from omegaconf import OmegaConf
+from wandb import Image as wandb_Image
+from wandb import Table
 
 import torch
+from torch.utils.data import Dataset, DataLoader
 from transformers import AutoProcessor, Blip2ForConditionalGeneration
 from train_blip2 import ImageCaptioningDataset
+from accelerate import Accelerator
 
-def get_images_stream(list_of_dirs):
-    for directory in list_of_dirs:
-        for filename in os.listdir(directory):
-            try:
-                path_to_img = os.path.join(directory, filename)
-                image = Image.open(path_to_img)
-                yield path_to_img, image
-            except:
-                continue
 
-def make_csv_and_save(paths, captions, config):
+class ImagesDataset(Dataset):
+    def __init__(self, list_of_dirs, processor):
+        
+        self.processor = processor
+        
+        self.image_paths = []
+        
+        for directory in list_of_dirs:
+            for filename in os.listdir(directory):
+                try:
+                    path_to_img = os.path.join(directory, filename)
+                    image = Image.open(path_to_img)
+                    image.close()
+                    self.image_paths.append(path_to_img)
+                except IOError:
+                    continue
+                except Exception as exc:
+                    raise
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image_path = self.image_paths[idx]
+        
+        try:
+            image = Image.open(image_path)
+                
+        except Exception as exc:
+            raise
+        
+        image_processed = self.processor(images=image, return_tensors="pt")       
+        image.close()
+        
+        
+        return {"pixel_values": image_processed.pixel_values.squeeze(), 
+                "image_path": image_path
+               }
+
+def collate_fn(batch):
+    processed_batch = {}
+    processed_batch["pixel_values"] = torch.stack([example["pixel_values"] for example in batch])
+    
+    processed_batch["image_path"] = []
+    for example in batch:
+        processed_batch["image_path"].extend(example["image_path"])
+
+    return processed_batch
+
+def make_accelerator(config):
+    
+    if config.wandb.enabled:
+        accelerator = Accelerator(log_with="wandb")
+
+        accelerator.init_trackers(
+            project_name=config.wandb.project, 
+            config=OmegaConf.to_container(config, resolve=True, throw_on_missing=True)
+        )
+    else:
+        accelerator = Accelerator()            
+        
+    return accelerator
+
+def load_model(config):
+    processor = AutoProcessor.from_pretrained(config.base_model_checkpoint, device_map="cpu")
+    model = Blip2ForConditionalGeneration.from_pretrained(config.base_model_checkpoint, device_map="cpu")
+    model.load_adapter(config.peft_checkpoint)
+    
+    return model, processor
+
+def make_csv_and_log(accelerator, paths, captions, config):
     df = pd.DataFrame(columns=['paths', 'caption'])
     df.paths = paths
     df.caption = captions
     
-    csv_name = f"{wandb.run.id}_{wandb.run.name}_" if config.wandb.enabled else ""
+    wandb_tracker = accelerator.get_tracker("wandb", unwrap=True)
+    csv_name = f"{wandb_tracker.id}_{wandb_tracker.name}_" if config.wandb.enabled else ""
     csv_name = csv_name + config.resulting_csv.base_name
     df.to_csv(os.path.join(config.resulting_csv.directory, csv_name))
+    
+    if config.wandb.enabled and config.wandb.log_visual_examples:
+        
+        table_data = []
+        for img_id, (caption, path_to_img) in enumerate(zip(captions, paths)):
+            if img_id == config.wandb.n_logged_visual_examples:
+                break
+                
+            img = Image.open(path_to_img).resize(tuple(config.wandb.img_loging_shape))
+            table_data.append([caption, wandb_Image(img)])
+           
+        
+        table = Table(data=table_data, columns=["generated_caption", "image"])
+
+        wandb_tracker.log_artifact(table)
                 
 @hydra.main(version_base=None, config_path="../configs", config_name="predict_blip2_config")
 def main(cfg):
     
-    if cfg.wandb.enabled:
-        wandb.config = OmegaConf.to_container(
-            cfg, resolve=True, throw_on_missing=True
-        )
+    accelerator = make_accelerator(cfg)
+    model, processor = load_model(cfg)
+    
+    dataset = ImagesDataset(cfg.image_folders_list, processor)
+    
+    dataloader = DataLoader(dataset,
+                          batch_size=cfg.dataloader.batch_size_per_gpu,
+                          num_workers=cfg.dataloader.n_workers,
+                          collate_fn=collate_fn)
+    
+    model, dataloader = accelerator.prepare(model, dataloader)
         
-        wandb.init(project=cfg.wandb.project)
-    
-    with torch.cuda.device(cfg.gpu):
-        processor = AutoProcessor.from_pretrained(cfg.base_model_checkpoint)
-        model = Blip2ForConditionalGeneration.from_pretrained(cfg.base_model_checkpoint, device_map="auto")
-        model.load_adapter(cfg.peft_checkpoint)
-    
     collected_paths = []
     collected_captions = []
-    data_img_log = []
     
-    for img_id, (img_path, image) in tqdm(enumerate(get_images_stream(cfg.image_folders_list))):
-        
-        img_processed = processor(images=image, return_tensors="pt").to(f"cuda:{cfg.gpu}", torch.float16)
-        generated_ids = model.generate(pixel_values=img_processed.pixel_values, max_length=100)
-        generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-        
-        collected_paths.append(img_path)
-        collected_captions.append(generated_caption)
-        
-        if img_id < cfg.wandb.n_logged_visual_examples:
-            data_img_log.append([generated_caption, image.resize(tuple(cfg.wandb.img_loging_shape))])
-        
-        image.close()
-        
+    progress_bar = tqdm(dataloader, desc="Generating captions...", leave=True,
+                            disable=not accelerator.is_main_process)
     
-    make_csv_and_save(collected_paths, collected_captions, cfg)
-    
-    if cfg.wandb.enabled and cfg.wandb.log_visual_examples:
+    for batch_id, batch in enumerate(progress_bar):
         
-        table = wandb.Table(data=list(map(lambda x: [x[0], wandb.Image(x[1])], data_img_log)), 
-                            columns=["generated_caption", "image"])
+        generated_ids = model.generate(pixel_values=batch["pixel_values"], max_length=cfg.generation.max_length)
+        generated_captions = list(processor.batch_decode(generated_ids, skip_special_tokens=True))
         
-        wandb.run.log({"table": table})
+        collected_paths.extend(batch["image_path"])
+        collected_captions.extend(generated_captions)
+        
+    if accelerator.is_main_process:
+        make_csv_and_log(accelerator, collected_paths, collected_captions, cfg)
 
 
 if __name__ == '__main__':
